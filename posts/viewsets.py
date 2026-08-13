a72c75d83828ae07236b828394d026f4d5f18165
mhmdin <mhmdin2007@gmail.com>
2026-08-13 23:47:37 +0330

    feat(api): add PostViewSet with ModelViewSet for API endpoints

---
+++ posts/viewsets.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = PostSerializer.objects.all()
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
