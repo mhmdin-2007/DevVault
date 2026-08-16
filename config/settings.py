78b433e565bb59b935d66cf5a073d5502be79a3f
mhmdin <mhmdin2007@gmail.com>
2026-08-16 23:35:29 +0330

    fix: Resolve docker dependency conflict.

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
+    'django.contrib.contenttypes', 
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
