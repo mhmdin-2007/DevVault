8401adbf9aabf2272bf3dcd99874190383264b23
mhmdin <mhmdin2007@gmail.com>
2026-09-10 13:39:32 +0330

    fix(posts): restrict post modifications to owners

--- posts/viewsets.py
+++ posts/viewsets.py
@@ -2,8 +2,26 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import BasePermission
+
+class IsOwnerOrReadOnly(BasePermission):
+    '''
+    Allow read access to everyone, but only the owner of an object can modify or delete it.
+    '''
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ("GET", "HEAD", "OPTIONS"):
+            return True
+
+        return obj.author == request.user
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
