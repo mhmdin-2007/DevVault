703a6f9187e7e2ee99d2b4d97f36cb1a184dba65
mhmdin <mhmdin2007@gmail.com>
2026-09-10 14:21:36 +0330

    fix(accounts): make user API read only

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 
-class UserViewset(viewsets.ModelViewSet):
+class UserViewset(viewsets.ReadOnlyModelViewSet):
     """API endpoint for users."""
     
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
